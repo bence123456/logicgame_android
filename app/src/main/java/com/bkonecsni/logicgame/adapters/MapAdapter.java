9dc2ed2ed4884e7350cb01056163093a304bbe90
Bence Konecsni <konecsni_bence@hotmail.com>
2018-11-26 17:50:53 +0100

    adaptive height for tiles in map

--- app/src/main/java/com/bkonecsni/logicgame/adapters/MapAdapter.java
+++ app/src/main/java/com/bkonecsni/logicgame/adapters/MapAdapter.java
@@ -1,8 +1,10 @@
 package com.bkonecsni.logicgame.adapters;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Icon;
 import android.support.annotation.NonNull;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -77,5 +79,24 @@ public class MapAdapter extends BaseAdapter {
 
     public void setTileList(List<TileBase> tileList) {
         this.tileList = tileList;
+    }
+
+    class MapTileItem extends android.support.v7.widget.AppCompatImageView {
+        public MapTileItem(Context context) {
+            super(context);
+        }
+
+        public MapTileItem(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public MapTileItem(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+        }
+
+        @Override
+        public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        }
     }
 }
